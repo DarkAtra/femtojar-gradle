43c0ae67aa46fbbf55cd15db1d6a62c97d881d6f
Tommy Schmidt <tommy.schmidt@idealo.de>
2026-07-28 22:14:33 +0200

    chore: add github workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlin.jvm)
     id("java-gradle-plugin")
-    id("maven-publish")
+    alias(libs.plugins.gradle.maven.publish)
 }
 
 group = "de.darkatra"
-version = "0.1.0"
+version = "dev-SNAPSHOT"
 
 java {
     targetCompatibility = JavaVersion.VERSION_17
@@ -45,14 +45,46 @@ gradlePlugin {
     }
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "github-packages"
-            url = uri("https://maven.pkg.github.com/DarkAtra/femtojar-gradle")
-            credentials {
-                password = System.getenv("GITHUB_TOKEN")
+val releaseVersion = providers.environmentVariable("RELEASE_VERSION")
+if (releaseVersion.isPresent) {
+    mavenPublishing {
+        coordinates(
+            groupId = project.group.toString(),
+            artifactId = project.name,
+            version = releaseVersion.get()
+        )
+
+        pom {
+            name.set(project.name)
+            description.set("A Gradle plugin for parttimenerd/femtojar.")
+            url.set("https://github.com/DarkAtra/femtojar-gradle")
+
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://opensource.org/license/mit")
+                    distribution.set("repo")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("DarkAtra")
+                    name.set("Tommy Schmidt")
+                    email.set("darkatra@gmail.com")
+                    url.set("https://github.com/DarkAtra")
+                    roles.add("maintainer")
+                }
+            }
+
+            scm {
+                connection.set("scm:git:https://github.com/DarkAtra/femtojar-gradle.git")
+                developerConnection.set("scm:git:ssh://git@github.com/DarkAtra/femtojar-gradle.git")
+                url.set("https://github.com/DarkAtra/femtojar-gradle")
             }
         }
+
+        signAllPublications()
+        publishToMavenCentral()
     }
 }
